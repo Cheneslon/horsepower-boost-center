7e285a10774d84806e5e68d62c35290e20aa1972
SWAG OVERLORD <marcusfenixdubstep@gmail.com>
2016-11-08 15:43:37 -0800

    refactored carService to convert Enums - JM

--- src/HorsePowerStore/Services/CarsService.cs
+++ src/HorsePowerStore/Services/CarsService.cs
@@ -42,15 +42,37 @@ namespace HorsePowerStore.Services
                 select cGroup.Key).ToList();
         }
 
-        public List<Car> ListFiltered (string make, string model, int year)
+        public List<_Car> ListFiltered (string make, string model, int year)
         {
-            return (
+            List<_Car> newCars = new List<_Car> { };
+
+            var tempCars =  (
                 from c in db.Cars
                 where 
                     c.Make == make && 
                     c.Model == model && 
                     c.Year == year
                 select c).ToList();
+
+            for (var x = 0; x < tempCars.Count; x++)
+            {
+
+                var holder = new _Car 
+                {
+                    Id = tempCars[x].Id,
+                    Make = tempCars[x].Make,
+                    Model = tempCars[x].Model,
+                    Year = tempCars[x].Year,
+                    Doors = tempCars[x].Doors,
+                    Drive = Enum.GetName(typeof(Drive), tempCars[x].Drive),
+                    Transmission = Enum.GetName(typeof(Transmission), tempCars[x].Transmission),
+                    Fuel = Enum.GetName(typeof(Fuel), tempCars[x].Fuel),
+                    CompatibleEngines = tempCars[x].CompatibleEngines
+                };
+                newCars.Add(holder);
+            }
+
+            return newCars;
         }
     }
 }
