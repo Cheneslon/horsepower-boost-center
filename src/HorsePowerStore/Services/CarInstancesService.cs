f4953981402365bcbfb62b02bb10ced0d1a024b3
CJ Olsen <chrj.olsen@gmail.com>
2016-11-14 14:25:17 -0800

    Added CarInstance API and client-side views/controllers/services. Linked new scripts in index.cshtml

---
+++ src/HorsePowerStore/Services/CarInstancesService.cs
@@ -0,0 +1,51 @@
+﻿using HorsePowerStore.Data;
+using HorsePowerStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorsePowerStore.Services
+{
+    public class CarInstancesService
+    {
+        private ApplicationDbContext appDbContext;
+
+        private ApplicationUser GetUser (string name)
+        {
+            return (
+                from u in appDbContext.AppUsers
+                where u.UserName == name
+                select u)
+                .Include(u => u.CarInstances)
+                .FirstOrDefault();
+        }
+
+        public CarInstancesService (ApplicationDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public void Save (CarInstance instance, string userName)
+        {
+            GetUser(userName).CarInstances.Add(instance);
+            appDbContext.SaveChanges();
+        }
+
+        public void Delete(int instanceId, string userName)
+        {
+            GetUser(userName).CarInstances.Remove((
+                    from ci in appDbContext.CarInstances
+                    where ci.Id == instanceId
+                    select ci)
+                .FirstOrDefault());
+            appDbContext.SaveChanges();
+        }
+
+        public List<CarInstance> GetUserSaved (string userName)
+        {
+            return GetUser(userName).CarInstances;
+        }
+    }
+}
---
+++ src/HorsePowerStore/wwwroot/ngApp/services/carInstanceService.ts
@@ -0,0 +1,11 @@
+﻿namespace HorsePowerStore.Controllers {
+    export class CarInstanceController {
+        public carInstances;
+
+        constructor(private carInstanceService: HorsePowerStore.Services.CarInstanceService) {
+            carInstanceService.listCarInstances().$promise.then((carInstances) => {
+                this.carInstances = carInstances;
+            })
+        }
+    }
+}
