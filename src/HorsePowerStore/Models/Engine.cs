121c386750a35de31b64a81159d9ffe4162106b6
CJ Olsen <chrj.olsen@gmail.com>
2016-11-14 11:17:06 -0800

    Changed intake type

--- src/HorsePowerStore/Models/Engine.cs
+++ src/HorsePowerStore/Models/Engine.cs
@@ -5,12 +5,19 @@ using System.Threading.Tasks;
 
 namespace HorsePowerStore.Models
 {
+    public enum Intake
+    {
+        SuperCharged,
+        TurboCharged,
+        NaturallyAspirated
+    }
+
     public class Engine
     {
         public int Id { get; set; }
         public int Cylinders { get; set; }
         public decimal Volume { get; set; }
-        public decimal Intake { get; set; }
+        public Intake Intake { get; set; }
         public decimal HorsePower { get; set; }
     }
 }
